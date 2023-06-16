1890db5a212772554c24b160a23ca459cbaedf49
Ben Gosney <bengosney@googlemail.com>
2023-06-16 17:21:22 +0100

    Suppressing decorators

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,10 @@
 # Third Party
 import keyring
+from keyring.errors import KeyringLocked
 
 # First Party
 from config import api_config
+from utils import suppress
 
 
 class Auth:
@@ -13,20 +15,24 @@ class Auth:
         return keyring.get_credential(api_config.service_name, None)
 
     @property
+    @suppress(KeyringLocked)
     def username(self) -> str | None:
         if credentials := self._credentials():
             return credentials.username
         return None
 
     @property
+    @suppress(KeyringLocked)
     def password(self) -> str | None:
         if credentials := self._credentials():
             return credentials.password
         return None
 
+    @suppress(KeyringLocked)
     def store(self, username: str, password: str) -> None:
         keyring.set_password(api_config.service_name, username, password)
 
+    @suppress(KeyringLocked)
     def remove(self) -> None:
         if self.username:
             keyring.delete_password(api_config.service_name, self.username)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 # Standard Library
+import functools
+from contextlib import suppress as ctx_suppress
 from typing import Any
 
 
@@ -13,3 +15,36 @@ def extract(store: dict[str, Any], selector: str) -> Any:
 
 def join_url(*args) -> str:
     return "/".join([str(p).strip("/") for p in args])
+
+
+class suppress_disable:
+    def __init__(self, func, exception, disabled_return=None) -> None:
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.exception = exception
+        self.disabled_return = disabled_return
+        self.enabled = True
+
+    def __call__(self, *args, **kwargs):
+        if self.enabled:
+            try:
+                return self.func(*args, **kwargs)
+            except self.exception:
+                self.enabled = False
+
+        return self.disabled_return
+
+
+def suppress(exception):
+    def decorator_suppress(func):
+        @functools.wraps(func)
+        def wrapper_suppress(*args, **kwargs):
+            with ctx_suppress(exception):
+                try:
+                    return func(*args, **kwargs)
+                except exception:
+                    return None
+
+        return wrapper_suppress
+
+    return decorator_suppress
