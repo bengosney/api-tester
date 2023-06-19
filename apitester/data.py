f3bd44465252e5828ef8dc6e4862a51fceffa4c5
Ben Gosney <bengosney@googlemail.com>
2023-06-19 17:29:34 +0100

    start of local storage

---
+++ apitester/data.py
@@ -0,0 +1,18 @@
+# Third Party
+from sqlmodel import Field, SQLModel, create_engine
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
+    power: str
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+SQLModel.metadata.create_all(engine)
