824fb47f59aeaca997f4af13ed1a76973c97d49c
Ben Gosney <bengosney@googlemail.com>
2023-08-23 13:56:14 +0100

    Move more forms to the modal form

--- apitester/app/screens/api_key.py
+++ apitester/app/screens/api_key.py
@@ -1,13 +1,11 @@
 # Third Party
 from pydantic import BaseModel
-from textual.app import ComposeResult
-from textual.containers import Grid
-from textual.screen import ModalScreen
-from textual.widgets import Button
 
 # First Party
 from apitester.auth import auth
-from apitester.widgets import Form
+
+# Locals
+from .modal_form import ModalFormScreen
 
 
 class APIKey(BaseModel):
@@ -15,25 +13,13 @@ class APIKey(BaseModel):
     remember: bool = True
 
 
-class APIKeyScreen(ModalScreen[bool]):
-    BINDINGS = [("escape", "app.pop_screen", "Cancel")]
+class APIKeyScreen(ModalFormScreen[APIKey]):
+    model = APIKey
 
-    def compose(self) -> ComposeResult:
-        self.form = Form[APIKey](APIKey, show_submit=False, id="api_key_form")
-        with Grid(id="dialog"):
-            yield self.form
-            with Grid(id="dialog-buttons"):
-                yield Button("Auth", variant="primary", id="do_auth")
-                yield Button("Cancel", id="cancel")
-
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "do_auth":
-            if api_key := self.form.submit():
-                if api_key.remember:
-                    auth.store("api_key", api_key.api_key)
-                else:
-                    auth.remove()
-                auth["api_key"] = api_key.api_key
-                self.dismiss(True)
+    def on_submit(self, model: APIKey) -> None:
+        if model.remember:
+            auth.store("api_key", model.api_key)
         else:
-            self.dismiss(False)
+            auth.remove()
+
+        auth["api_key"] = model.api_key
--- apitester/app/screens/login.py
+++ apitester/app/screens/login.py
@@ -1,7 +1,11 @@
 # Standard Library
 
+# Standard Library
+from typing import Any
+
 # Third Party
 import aiohttp
+from pydantic import BaseModel
 from textual.app import ComposeResult
 from textual.containers import Grid
 from textual.screen import ModalScreen
@@ -12,8 +16,44 @@ from apitester.auth import auth
 from apitester.config import BearerAuthConf
 from apitester.utils import extract
 
+# Locals
+from .modal_form import ModalFormScreen
 
-class LoginScreen(ModalScreen):
+
+class LoginModel(BaseModel):
+    username: str
+    password: str
+    remember: bool = True
+
+
+class LoginScreen(ModalFormScreen[LoginModel]):
+    model = LoginModel
+
+    def __init__(self, auth: BearerAuthConf, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.auth = auth
+
+    @property
+    def inital(self) -> dict[str, Any]:
+        return {"username": auth.username, "password": auth.password}
+
+    async def on_submit(self, model: LoginModel) -> None:
+        if model.remember:
+            auth.store(model.username, model.password)
+        else:
+            auth.remove()
+
+        async with aiohttp.ClientSession() as session:
+            post_data = {
+                "username": model.username,
+                "password": model.password,
+            }
+            async with session.post(self.auth.url, data={**post_data}) as response:
+                json = await response.json()
+                auth["token"] = extract(json, self.auth.token_path)
+
+
+class LoginScreenOLD(ModalScreen):
     BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
 
     def __init__(self, auth: BearerAuthConf, *args, **kwargs) -> None:
