e37a2b0d1ff587233a0cacb96fb8817acbde30df
Ben Gosney <bengosney@googlemail.com>
2023-08-23 14:15:36 +0100

    Remove old login screen

--- apitester/app/screens/login.py
+++ apitester/app/screens/login.py
@@ -6,10 +6,6 @@ from typing import Any
 # Third Party
 import aiohttp
 from pydantic import BaseModel
-from textual.app import ComposeResult
-from textual.containers import Grid
-from textual.screen import ModalScreen
-from textual.widgets import Button, Checkbox, Input, Label
 
 # First Party
 from apitester.auth import auth
@@ -51,47 +47,3 @@ class LoginScreen(ModalFormScreen[LoginModel]):
             async with session.post(self.auth.url, data={**post_data}) as response:
                 json = await response.json()
                 auth["token"] = extract(json, self.auth.token_path)
-
-
-class LoginScreenOLD(ModalScreen):
-    BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
-
-    def __init__(self, auth: BearerAuthConf, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.auth = auth
-
-    def compose(self) -> ComposeResult:
-        self.username_input = Input(id="username", value=auth.username)
-        self.password_input = Input(id="password", password=True, value=auth.password)
-        self.remember = Checkbox(id="remember", value=True)
-
-        with Grid(id="dialog"):
-            with Grid(id="dialog-inputs"):
-                yield Label("Get Auth Token", id="question")
-                yield Label("Username")
-                yield self.username_input
-                yield Label("Password")
-                yield self.password_input
-                yield Label("Remember me")
-                yield self.remember
-            with Grid(id="dialog-buttons"):
-                yield Button("Auth", variant="primary", id="do_auth")
-                yield Button("Cancel", id="cancel")
-
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "do_auth":
-            if self.remember.value:
-                auth.store(self.username_input.value, self.password_input.value)
-            else:
-                auth.remove()
-
-            async with aiohttp.ClientSession() as session:
-                post_data = {
-                    "username": self.username_input.value,
-                    "password": self.password_input.value,
-                }
-                async with session.post(self.auth.url, data={**post_data}) as response:
-                    json = await response.json()
-                    auth["token"] = extract(json, self.auth.token_path)
-
-        self.dismiss()
