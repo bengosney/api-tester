c7a59111ab824078068a9cc750f21d1cc1883979
Ben Gosney <bengosney@googlemail.com>
2023-09-15 11:06:19 +0100

    WIP: Update the plugin list to be a datatable

--- apitester/app/screens/plugin_list.py
+++ apitester/app/screens/plugin_list.py
@@ -2,7 +2,7 @@
 from textual.app import ComposeResult
 from textual.containers import VerticalScroll
 from textual.screen import ModalScreen
-from textual.widgets import Button, Label
+from textual.widgets import Button, DataTable, Label
 
 # First Party
 from apitester.plugin_manager import PluginManager
@@ -15,14 +15,21 @@ class PluginScreen(ModalScreen[None]):
     AUTO_FOCUS = "Button"
 
     def compose(self) -> ComposeResult:
-        plugins = getattr(self.app, "plugin_manager", PluginManager(self.log))
-
         with VerticalScroll(id="dialog"):
             yield Label("Active Plugins", classes="title")
-            with VerticalScroll(id="list"):
-                for name, description in plugins.active_plugins:
-                    yield Label(f"{name} - {description or 'no description'}")
+            yield DataTable(id="plugin-list")
             yield Button("Close", id="close")
+
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+
+        table.add_columns("Name", "Description")
+
+        plugins = getattr(self.app, "plugin_manager", PluginManager(self.log))
+
+        for name, description in plugins.active_plugins:
+            self.log(f"plugin {name} - {description}")
+            table.add_row(name, description)
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         self.dismiss()
